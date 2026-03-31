c99ea4a2a60fbb3e26c3e4d9eae63c1121ad28b6
Mark Gerald Pacun <markgpacun@gmail.com>
2026-03-31 23:48:33 +0800

    Robust Flow of the App. Create new experience to the user.

--- app/src/main/java/com/example/edulock/utils/AuthStateManager.java
+++ app/src/main/java/com/example/edulock/utils/AuthStateManager.java
@@ -1,5 +1,17 @@
 package com.example.edulock.utils;
 
-public class AuthStateManager {
+import android.content.Context;
+import android.content.SharedPreferences;
 
+public class AuthStateManager {
+    private static final String PREFS_NAME = "edulock_auth_state";
+    private static final String KEY_WELCOME_COMPLETED = "welcome_completed";
+    private static final String KEY_PERMISSION_GRANTED = "permission_granted";
+    private static final String KEY_USER_LOGGED_IN = "user_logged_in";
+
+    private final SharedPreferences prefs;
+
+    public AuthStateManager(Context context) {
+        prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+    }
 }
